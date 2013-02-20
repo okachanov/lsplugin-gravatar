fb61c9cf0f636d5c303636e050133f47cf7b4942
al.germis <al.germis@gmail.com>
2013-02-20 15:16:10 +0200

    Initial

--- PluginGravatar.class.php
+++ PluginGravatar.class.php
@@ -1,23 +1,20 @@
 <?php
 
-class PluginGravatar extends Plugin {
-
-	//*********************************************************************************	
-    protected $aInherits = array(
-		'entity'  => array('ModuleUser_EntityUser'=>'_ModuleGravatar_EntityUser')
+class PluginGravatar extends Plugin
+{
+    protected $aInherits=array(
+       'entity'  =>array('ModuleUser_EntityUser'=>'_ModuleGravatar_EntityUser')
     );
-	
-	//*********************************************************************************	
+
 	public function Activate(){
 		return true;
 	}
 	
-	//*********************************************************************************	
+
 	public function Deactivate(){
 		return true;
 	}
 	
-	//*********************************************************************************	
 	public function Init(){
 	}
 	
